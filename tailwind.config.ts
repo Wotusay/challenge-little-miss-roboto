0104616ccc4d4604acbcf950b763d992dcb7c106
Wout Salembier <HA-SalemWou@electrolux.com>
2024-01-24 10:09:25 +0100

    chore: added new starting files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,57 @@
-import type { Config } from "tailwindcss";
+import { nextui } from '@nextui-org/react';
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/containers/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                wrapper: '#0C6AAE',
+                cards: '#0E325B',
+            },
+            fontFamily: {
+                sans: ['var(--font-flanders-art)'],
+            },
+            boxShadow: {
+                'inset-black-25': '0px -3px 0px 0px rgba(0, 0, 0, 0.25) inset',
+            },
+        },
     },
-  },
-  plugins: [],
+    darkMode: 'class',
+    plugins: [
+        nextui({
+            addCommonColors: true,
+            defaultTheme: 'light',
+            defaultExtendTheme: 'light',
+            themes: {
+                light: {
+                    colors: {
+                        background: '#004475',
+                        primary: {
+                            foreground: '#004475',
+                            DEFAULT: '#FFE200',
+                        },
+                        divider: '#0C6AAE',
+                        overlay: '#0C6AAE',
+                        secondary: {
+                            foreground: '#004475',
+                            DEFAULT: '#FFFFFF',
+                        },
+                        foreground: '#004475',
+                        default: {
+                            foreground: '#FFFFFF',
+                            DEFAULT: '#0E325B',
+                        },
+                    },
+                },
+            },
+        }),
+    ],
 };
 export default config;
